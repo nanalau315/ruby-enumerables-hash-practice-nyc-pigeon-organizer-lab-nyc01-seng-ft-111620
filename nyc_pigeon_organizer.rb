fbb3cc619a1c303cc269ed12355ab70537024c2d
Nana Lau <nanalau315@hotmail.com>
2020-11-09 02:51:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,7 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  
+  
   data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
       names.each do |name|
